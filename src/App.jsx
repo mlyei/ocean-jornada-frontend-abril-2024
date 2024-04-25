9078784fa05d255030f675cc1c3358294f8e93d7
Emily <124410773+mlyei@users.noreply.github.com>
2024-04-25 23:46:50 +0000

    #mudança no nome da pagina #mudança no nome da pasta #mudança nome da pasta

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+    tags: ['Status: Vivo']
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+    tags: ['Espécie: Humana']
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      <div className="cards">
+        {itens.map(function (elemento) {
+          return <Card item={elemento} />
+        })}
       </div>
     </>
   )
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import Tag from "../Tag/Tag"
+
+export default function Card(props) {
+    const item = props.item
+
+    return (
+        <div className="card">
+            <h2>{item.name}</h2>
+
+            <div className="tags">
+                {item.tags.map(function (tag) {
+                    return <Tag text={tag} />
+                })}
+            </div>
+
+            <img src={item.image} />
+        </div>
+    )
+}
